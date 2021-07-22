8d7ce5a9ae11c0dcaa2a35fe593518070325c911
HanKunChai <56723313+HanKunChai@users.noreply.github.com>
2021-07-22 19:39:47 +0800

    something

--- model_test.py
+++ model_test.py
@@ -1,6 +1,6 @@
 from Model.model import get_model
 if __name__ == '__main__':
-    model = get_model(name="CBMA-PyResNet")
+    model = get_model(name="SE-PyResNet")
     model.summary()
     import sys
     sys.exit()
